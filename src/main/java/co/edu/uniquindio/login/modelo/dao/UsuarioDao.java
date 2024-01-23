e809545808a4b4d567543ad50db99c5cc29dd00c
repoYan <yan_cristiano@hotmail.com>
2024-01-22 19:48:23 -0500

    Se creo el usuario y el usuario dao, en el usuario dao lo que se realizo fue el crud de cada usuario

--- src/main/java/co/edu/uniquindio/login/modelo/dao/UsuarioDao.java
+++ src/main/java/co/edu/uniquindio/login/modelo/dao/UsuarioDao.java
@@ -19,29 +19,71 @@ public class UsuarioDao {
      */
     public int buscar(String usuario)
     {
-        return 0;
+        int n = -1;
+        for (int i = 0; i <usuarios.size() ; i++) {
+            if(usuarios.get(i).getUser().equals(usuario)) {
+                n = i;//posición en la que encontró el elemento
+                break;
+            }
+        }
+        return n;
     }
-    //crea un usuario y lo almacena en el arraylist
+    /*almacena un usuario que le llega por parámetro al método y
+     verifica en el if() que retorna el método buscar, si retorna -1 quiere
+     decir que el usuario no está en el arraylist entonces ingresa dentro del
+     if() y agrega al usuario al arraylist y retorna true, sino, retorna false,
+     dando a entender que no se realizó inserción.
+    */
     public boolean insertar(Usuario user)
     {
-        return true;
+        if(buscar(user.getUser()) == -1){
+            usuarios.add(user);
+            return true;
+        }
+        else {
+            return  false;
+        }
     }
     public boolean modificar(Usuario user)
     {
-        return true;
+        if(buscar(user.getUser()) != -1){
+            Usuario userAux = obtener(user.getUser());
+
+            userAux.setPassword(user.getPassword());
+            userAux.setName(user.getName());
+            userAux.setLastName(user.getLastName());
+            userAux.setEmail(user.getEmail());
+
+            return true;
+        }
+        else {
+            return  false;
+        }
     }
-    public boolean eliminar(Usuario user)
+    public boolean eliminar(String user)
     {
-        return true;
+        if(buscar(user) != -1){
+            usuarios.remove(buscar(user));
+            return true;
+        }
+        else {
+            return  false;
+        }
     }
 
     /**
-     * Busca en la lista de usuarios, mira en que posición está
-     * algún elemento y lo retorna.
+     * Este método verifica si el método buscar() es
+     * diferente de -1, si lo es, quiere decir que el objeto se
+     * encuentra en el arraylist y lo retornaría, si no, retorna null;
      */
-    public Usuario obtener(Usuario user)
+    public Usuario obtener(String user)
     {
-        return null;
+       if(buscar(user) != -1){
+            return usuarios.get(buscar(user));//retornamos la posición en la que se encontró al usuario
+       }
+       else {
+            return null;
+       }
     }
 
 }
