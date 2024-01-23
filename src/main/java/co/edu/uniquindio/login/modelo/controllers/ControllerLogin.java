c2932ff413a87cb9ed0e4e5785c29559fe8c6328
repoYan <yan_cristiano@hotmail.com>
2024-01-23 15:44:52 -0500

    Se corriegieron algunos errores

--- src/main/java/co/edu/uniquindio/login/modelo/controllers/ControllerLogin.java
+++ src/main/java/co/edu/uniquindio/login/modelo/controllers/ControllerLogin.java
@@ -1,19 +1,39 @@
 package co.edu.uniquindio.login.modelo.controllers;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerLogin implements Initializable {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button BtnRegistrarUsuario;
+
+    @FXML
+    private TextField txtUserIngresar;
+
+    @FXML
+    private PasswordField txtPasswordIngresar;
+
+    @FXML
+    private Button btnIngresarUsuario;
+
+    @FXML
+    private CheckBox cbxMostrarPassword;
+
+    @FXML
+    void btnIngresarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnRegistrarUsuario(ActionEvent event) {
+
     }
 
     @Override
